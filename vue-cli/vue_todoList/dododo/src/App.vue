ab43770baff5bdea8b43a6ac00119bcd4013592a
andbreak <andbreak@outlook.com>
2021-07-09 18:03:04 +0800

    提交

---
+++ vue-cli/vue_todoList/dododo/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header></Header>
+      <List :todos='todos'></List>
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header/Header.vue';
+import List from './components/List/List.vue';
+import Footer from './components/Footer/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    List,
+    Footer
+  },
+  data(){
+    return {
+        // 初始时就读取json数据并解析为js的数组, 如果没有指定为空数组
+        todos: [
+          {id: this.setIdWithTimer, title: '阿拉斯加海湾', isChecked: false},
+          {id: this.setIdWithTimer, title: '沉淀', isChecked: false},
+          {id: this.setIdWithTimer, title: 'Flower Dance', isChecked: false},
+        ]
+    }
+  },
+  methods: {
+    setIdWithTimer(){
+      return Date.now();
+    }
+  }
+}
+</script>
+
+<style>
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ vue-cli/vue_todoList/dododo/src/components/List/List.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="todo-main">
+        <Item></Item>
+    </ul>
+</template>
+
+<script>
+import Item from '../Item/Item.vue'
+
+export default {
+    name: 'List',
+    components: {
+        Item
+    }
+}
+</script>
+
+<style scope>
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
