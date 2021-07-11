142c8d9ce5b08e0da752473d69c08c5ab43eeb95
andbreak <andbreak@outlook.com>
2021-07-11 22:27:42 +0800

    重新配置文件结构

--- vue-cli/vue_todoList/dododo/src/App.vue
+++ vue-cli/vue_todoList/dododo/src/App.vue
@@ -12,6 +12,7 @@
 import Header from './components/Header/Header.vue';
 import List from './components/List/List.vue';
 import Footer from './components/Footer/Footer.vue';
+// import Navigationlink from './components/Navigation-link/Navigation.vue';
 import { v4 as uuidv4 } from 'uuid';
 
 export default {
